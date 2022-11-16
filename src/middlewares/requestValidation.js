3b27af911cdeb7515521f7dcae787b50ef8f58bc
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-11-16 15:37:29 -0300

    implementing JOI inside validation middleware

--- src/middlewares/requestValidation.js
+++ src/middlewares/requestValidation.js
@@ -1,14 +1,25 @@
+const joi = require('joi');
+
 const BAD_REQUEST = 400;
 const UNPROCESSABLE_ENTITY = 422;
 
-const validateProduct = ({ body: { name } }, res, next) => {
-  if (name === undefined) {
-    return res.status(BAD_REQUEST).json({ message: '"name" is required' });
+const productSchema = joi.object({
+  name: joi.string()
+    .alphanum()
+    .min(5)
+    .required(),
+});
+
+const validateProduct = ({ body }, res, next) => {
+  const { error: { details } } = productSchema.validate(body);
+  const { message, type } = details[0];
+  if (type === 'any.required') {
+    return res.status(BAD_REQUEST).json({ message });
   }
-  if (name.length < 5) {
+  if (type === 'string.min') {
     return res
       .status(UNPROCESSABLE_ENTITY)
-      .json({ message: '"name" length must be at least 5 characters long' });
+      .json({ message });
   }
   next();
 };
