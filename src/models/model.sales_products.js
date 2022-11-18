270562b63043a356c29d7efc641c082beaaad140
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-11-18 19:34:21 -0300

    handling multiple asyncronous requests with Promise.all

--- src/models/model.sales_products.js
+++ src/models/model.sales_products.js
@@ -1,10 +1,14 @@
 const db = require('./connection');
 
 const add = async (body, saleId) => {
-  body.forEach(async ({ productId, quantity }) => {
-    await db.query(`INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
-    VALUES (?, ?, ?)`, [saleId, productId, quantity]);
+  const promissesStack = body.map(async ({ productId, quantity }) => {
+    await db.query(
+      `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+    VALUES (?, ?, ?)`,
+      [saleId, productId, quantity],
+    );
   });
+  return Promise.all(promissesStack);
 };
 
 module.exports = { add };
