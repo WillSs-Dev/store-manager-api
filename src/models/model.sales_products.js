33c4b1db0a54319ea26adb4616c3780a67d22c57
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-11-18 19:38:36 -0300

    refactoring async code to return promisses directly inside the body.map

--- src/models/model.sales_products.js
+++ src/models/model.sales_products.js
@@ -1,13 +1,11 @@
 const db = require('./connection');
 
 const add = async (body, saleId) => {
-  const promissesStack = body.map(async ({ productId, quantity }) => {
-    await db.query(
+  const promissesStack = body.map(({ productId, quantity }) => db.query(
       `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
     VALUES (?, ?, ?)`,
       [saleId, productId, quantity],
-    );
-  });
+    ));
   return Promise.all(promissesStack);
 };
 
